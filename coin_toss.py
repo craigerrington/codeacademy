04c5810295b769885591df684e1a1994dc891ca0
Craig Errington <craigerrington@gmail.com>
2019-11-20 21:25:49 +0000

    else not elif for last statement

--- coin_toss.py
+++ coin_toss.py
@@ -11,7 +11,7 @@ def coin_toss(call,bet):
     elif call == "Tails" and num == 1:
         print("Heads, you lost " + str(-bet))
         return -bet
-    elif call == "Heads" and num == 2:
+    else:
         print("Tails, you lost " + str(-bet))
 
 #Test
