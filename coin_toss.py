b1bb66296b966bc4786007bcce6ea71425ac4d3d
Craig Errington <craigerrington@gmail.com>
2019-11-20 21:23:25 +0000

    coin toss

---
+++ coin_toss.py
@@ -0,0 +1,18 @@
+import random
+
+def coin_toss(call,bet):
+    num = random.randint(1, 2)
+    if call == "Heads" and num == 1:
+        print("Heads, you won " + str(bet))
+        return bet
+    elif call == "Tails" and num == 2:
+        print("Tails, you won " + str(bet))
+        return bet
+    elif call == "Tails" and num == 1:
+        print("Heads, you lost " + str(-bet))
+        return -bet
+    elif call == "Heads" and num == 2:
+        print("Tails, you lost " + str(-bet))
+
+#Test
+coin_toss("Heads",25)
